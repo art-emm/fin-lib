be2093ddc07034c7e985b68929c0e7b7a17b78d2
Artem Livanov <art.fox.emm@gmail.com>
2024-02-17 12:50:10 +0200

    update lib

--- src/interfaces/api.ts
+++ src/interfaces/api.ts
@@ -30,3 +30,53 @@ export interface IApiUserInstrument {
     profitPercent: number;
     quantity: number;
 }
+
+export interface IApiStockInfo {
+    price: number;
+    grahams: {
+        eps: number;
+        pOnENoGrowth: number
+        growthRate: number
+        averageYealdAAABond: number
+        currentYealdAAACorporateBond: number
+    },
+    dcf: {
+        cashFlow:{ [year: string]: { cash: number; growthRate: number }}
+        averageGrowthRate: number;
+        cashAndCashEquivalents: number;
+        totalDebt: number;
+    },
+    wacc: {
+        costOfDebt: {
+            interestExpence: number;
+            totalDebt: number;
+            costOfDebt?: number;
+            incomeTaxExpense: number;
+            incomeBeforeTax: number;
+            effectiveTaxRate?: number;
+            costOfDebtAfterTax?: number;
+        },
+        costOfEquity: {
+            riskFreeRate?: number;
+            marketReturnRate?: number;
+            beta?: number;
+            costOfEquity?: number;
+        },
+        marketCap: number;
+    },
+    lynch: {
+        futureEpsGrowth: number;
+    },
+    }
+}
+
+export interface IApiSearchStock {
+    exchange: string
+    shortname: string
+    quoteType: string
+    symbol: string
+    score: number
+    longname: string
+    sector: string
+    industry: string
+}
--- src/interfaces/api/dashboard.ts
+++ src/interfaces/api/dashboard.ts
@@ -7,6 +7,7 @@ export interface IApiDashboardDeposit {
 }
 
 export interface IApiDashboardChart {
+    portfolioAvgBuyPrice: number[]
     portfolio: number[]
     timeline: string[]
 }
